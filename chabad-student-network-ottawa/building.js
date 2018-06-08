965bbcc617446c0a4944ed905721cacc5cfe2456
Mendel Konikov <mkonikov@gmail.com>
2018-06-08 01:06:56 -0400

    Document sheets to JSON info

--- chabad-student-network-ottawa/building.js
+++ chabad-student-network-ottawa/building.js
@@ -3,13 +3,14 @@
     jQuery(document).ready(function(e) {
       $.ajax({
         method: 'get',
+        // with thanks to https://sandbox.idre.ucla.edu/sandbox/general/databasing-google-spreadsheets-to-json
         url: `https://spreadsheets.google.com/feeds/list/1VqxIarzdTnAKcPoD9Xvx6r1VHvnSaH8fccL1woxQPjg/od6/public/values?alt=json`,
       })
       .then((data) => {
           var $latestDonors = $('#latest-donors');
           data.feed.entry.forEach((entry) => {
             var donor = entry.gsx$donors.$t;
-            var $li = $('<li/>')
+            var $li = $('<li/>');
             $li.text(donor)
             .appendTo($latestDonors);
           });
