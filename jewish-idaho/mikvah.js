75c55b6b0882544de515824b9e87d56a7efeb664
Mendel Konikov <mkonikov@gmail.com>
2018-07-08 23:07:28 -0400

    Idaho mikvah

---
+++ jewish-idaho/mikvah.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.location.href.indexOf("3954543") !== -1) {
+    document.querySelector(".chabad_left_column").style.backgroundPositionX =
+      "113%";
+  }
+});
