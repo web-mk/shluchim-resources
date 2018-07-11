54fc99f24581a483e3dd43930ef4416abf82b76d
Mendel Konikov <mkonikov@gmail.com>
2018-07-10 23:41:28 -0400

    Load bootstrap js after jquery

--- chabad-vt/building.js
+++ chabad-vt/building.js
@@ -7,7 +7,10 @@
         });
         jQuery('.footer3').prepend('<div><p>Mikvah Campaign & Chabad Expansion for Chabad of the Space & Treasure Coasts. Chabad is a 501(c)3 charity and all contributions to it are tax deductible.</p></div>');
         jQuery('.cco_templateless_template').remove();
-  
+
+        var script = document.createElement('script'); 
+        script.src = 'https://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js'; 
+        document.head.appendChild(script); 
       });
     } else {
       setTimeout(function() {
