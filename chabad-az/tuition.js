efb0fee5d190c25cad47bf057acd7a2f8d5e2493
Mendel Konikov <mkonikov@gmail.com>
2018-12-08 23:17:52 -0500

    Add ref code

--- chabad-az/tuition.js
+++ chabad-az/tuition.js
@@ -5,6 +5,19 @@ function runJQueryJs() {
   jQuery('.footer3').prepend(
     "<div>Notice: A school cannot award, restrict or reserve scholarships solely on the basis of donor recommendation. <br />A Taxpayer may not claim a tax credit if the taxpayer agrees to swap donations with another taxpayer to benefit either taxpayer's own dependent.<br /><br /></div>"
   );
+
+  if (window.location.href.indexOf('3261625') > -1) {
+    const hash = window.location.hash;
+    const $refLine = jQuery('#id_26');
+    if (hash.indexOf('#ref=') === 0) {
+      const code = hash.slice(5);
+      const $refInput = jQuery('#input_26');
+      $refInput.val(code);
+      $refLine.hide();
+    } else {
+      $refLine.hide();
+    }
+  }
 }
 
 (function defer() {
